13823c89d27c8c43e6b71ec3dcb5395ec8ed4616
Igor Batista <igorsb11@hotmail.com>
2022-01-26 19:26:58 -0300

    ng-content implemented

--- src/app/src/app/data-sharing/data-sharing.module.ts
+++ src/app/src/app/data-sharing/data-sharing.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { ParentComponentComponent } from './parent-component/parent-component.component'
 import { ChildComponentComponent } from './child-component/child-component.component'
+import { NgContentParentComponent } from './ng-content/ng-content-parent/ng-content-parent.component'
+import { NgContentChildComponent } from './ng-content/ng-content-child/ng-content-child.component'
 
 @NgModule({
-  declarations: [ParentComponentComponent, ChildComponentComponent],
+  declarations: [
+    ParentComponentComponent,
+    ChildComponentComponent,
+    NgContentParentComponent,
+    NgContentChildComponent,
+  ],
   imports: [CommonModule, NgbModule],
-  exports: [ParentComponentComponent, ChildComponentComponent],
+  exports: [
+    ParentComponentComponent,
+    ChildComponentComponent,
+    NgContentParentComponent,
+    NgContentChildComponent,
+  ],
 })
 export class DataSharingModule {}
